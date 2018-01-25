b385d8873b926d443314c033974ad2a62c21ab45
Dmitry Klimenko <dimael.vertigo@gmail.com>
2018-01-25 13:34:23 +0200

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <header className="App-header">
+          header
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
